1cb2354e0ea02ca8613da84cc6b8278e0be3e087
Fahad <fahad.zahid2004@gmail.com>
2026-03-28 15:25:23 +0000

    package file change

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/moallem-home-care/">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
